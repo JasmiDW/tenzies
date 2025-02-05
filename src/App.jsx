dc39e9113379183bd725eab170a4df7c92a291d9
Nadia Rousseau <nadia.rousseau@serli.com>
2025-02-05 13:59:58 +0100

    feat(add timer)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 /* eslint-disable react/no-unescaped-entities */
 import './App.css'
 import Die from './components/Die'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { nanoid } from "nanoid"
 import Confetti from 'react-confetti'
 
 function App() {
   const [dice, setDice] = useState(() => generateDice())
+  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(0);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+
+  useEffect(() => {
+    let timer;
+    if (isTimerRunning) {
+      timer = setInterval(() => {
+        setTime(prevTime => prevTime + 1);
+      }, 1000); // Le timer augmente chaque seconde
+    } else {
+      clearInterval(timer);
+    }
+    return () => clearInterval(timer);
+  }, [isTimerRunning]);
+
 
   let gameWon = (dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value))
- 
+
+
   function generateDice() {
     let dices = [];
   
@@ -24,6 +41,9 @@ function App() {
   }
 
   function holdDice(id){
+    if (!isTimerRunning) {
+      setIsTimerRunning(true);
+    }
     setDice(prevHold => prevHold.map(item => {
       return item.id === id ? {...item, isHeld : !item.isHeld} : item
     }))
@@ -39,12 +59,14 @@ function App() {
 
   function handleDice(){
     if (!gameWon){
+      setCount(count + 1)
       setDice(prevHold => prevHold.map(item => {
         return item.isHeld !== true ? {...item, value: Math.ceil(Math.random() * 6)} : item
         
       }))
     } else {
       setDice(generateDice())
+      
     }
   }
 
@@ -53,6 +75,8 @@ function App() {
       {gameWon && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Lancez les dés jusqu'à ce qu'ils soient tous identiques. Cliquez sur chaque dé pour le figer à sa valeur actuelle entre deux lancers.</p>
+      <p>Nombre de tour joués: {count}</p>
+      <div>Temps : {time} secondes</div>
       <div className='container'>
         {diceValue}
       </div>
